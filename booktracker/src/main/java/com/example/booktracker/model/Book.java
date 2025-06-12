3c82bd30b3727de3cae561ca433f7e3badfef54f
Matei-Ciofu <mateiciofu@gmail.com>
2025-06-12 22:55:33 +0200

    Creazione della Classe "Book"

---
+++ booktracker/src/main/java/com/example/booktracker/model/Book.java
@@ -0,0 +1,61 @@
+package com.example.booktracker.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String status; // LETTO / DA LEGGERE
+    private Integer rating;
+    private String notes;
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getRating() {
+        return this.rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getNotes() {
+        return this.notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+    
+}
